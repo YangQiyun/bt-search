fd31653da6e9832f01412cf6bcaa00cdfb47959b
YangQiyun <837167592@qq.com>
2018-05-18 19:31:02 +0800

    init

---
+++ src/main/java/com/edu/seu/BtSearchApplication.java
@@ -0,0 +1,12 @@
+package com.edu.seu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtSearchApplication.class, args);
+	}
+}
